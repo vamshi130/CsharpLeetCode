4707648aabe6818ab1a52f13c1f17df8bb417b4f
vamshi <vamshichowdary130@gmail.com>
2023-08-01 22:27:40 +0530

    Adding Letter Combination PhoneNumber recusion problem

---
+++ Recurssion/LetterCombinationOfaPhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurssion
+{
+    public static class LetterCombinationOfaPhoneNumber
+    {
+        public static void LetterCombination(string str, string ans)
+        {
+            if(str.Length == 0) { 
+                Console.WriteLine(ans); return; }
+            string[] keypad = new string[] { "" ,"abc", "bcd", "cef", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            Console.WriteLine((str[0] -48));
+            var key = keypad[str[0] - 48];
+            for(int i=0;i<key.Length;i++)
+            {
+                LetterCombination(str.Substring(1), ans + key[i]);
+            }
+        }
+    }
+}
---
+++ Recurssion/SubsequenceInAString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurssion
+{
+    public static class SubsequenceInAString
+    {
+        public static void SubSequence(string str, string ans)
+        {
+            if(str.Length == 0)
+            {
+                Console.WriteLine(ans);
+                return;
+            }
+            SubSequence(str.Substring(1), ans + str[0]);
+            SubSequence(str.Substring(1), ans);
+
+        }
+    }
+}
